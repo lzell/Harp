6030c650e1cfcf4afda14b0011ec27b83a775cca
Lou Zell <lzell11@gmail.com>
2016-06-23 17:21:15 -0700

    HarpClient

---
+++ HarpClient/HarpClient/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HarpClient
+//
+//  Created by Lou Zell on 6/23/16.
+//  Copyright © 2016 Lou Zell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
