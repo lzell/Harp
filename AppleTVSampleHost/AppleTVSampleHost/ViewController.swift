ff7bbe718ffd28c129d35704048b98e0a1e445e8
Lou Zell <lzell11@gmail.com>
2016-08-16 12:02:45 -0700

    Added AppleTV Sample Project

--- AppleTVSampleHost/AppleTVSampleHost/ViewController.swift
+++ AppleTVSampleHost/AppleTVSampleHost/ViewController.swift
@@ -1,25 +1,18 @@
-import Cocoa
-import HarpCommonOSX
+import UIKit
+import HarpCommonTvOS
 
-
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate, HarpServiceDelegate {
+class ViewController: UIViewController, HarpServiceDelegate {
 
     let service = HarpService(maxConcurrentConnections: 2)
+    @IBOutlet weak var textView : UITextView!
 
-    @IBOutlet weak var window: NSWindow!
-    @IBOutlet var textView: NSTextView!
-
-
-    // MARK: -
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
         service.delegate = self
         service.register()
         log("Started Service for \(service.maxConcurrentConnections) players, waiting for them to join...")
     }
-
-
+    
     // MARK: -
 
     @IBAction func switchToController1(sender: AnyObject) {
@@ -55,7 +48,8 @@ class AppDelegate: NSObject, NSApplicationDelegate, HarpServiceDelegate {
 
     private func log(msg: String) {
         print(msg)
-        textView.insertText(msg, replacementRange: textView.selectedRange())
-        textView.insertNewline(nil)
+        textView.insertText("\(msg)\n")
+        textView.scrollRangeToVisible(NSRange(location: textView.text.characters.count - 1, length: 0))
     }
 }
+
